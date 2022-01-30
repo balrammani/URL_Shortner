fcb6d08093d2faa078b1b2963d1ab07b79580b19
Balaraman Mani <bmani1@ext.uber.com>
2022-01-30 19:37:04 +0530

    added test for random generator string

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/balram/rest-url-shortner
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
--- utils/random_generator/random_generator_test.go
+++ utils/random_generator/random_generator_test.go
@@ -1,2 +1,53 @@
 package randomgenerator
 
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+type suite struct {
+	ctx  context.Context
+	ctrl *gomock.Controller
+	t    *testing.T
+
+	random RandomService
+	err    error
+}
+
+func (s *suite) setupTest() {
+	s.random, s.err = New()
+	assert.NoError(s.t, s.err)
+
+}
+
+func TestGenerateRandomString(t *testing.T) {
+	var s suite
+
+	scenarios := []struct {
+		name  string
+		setup func()
+	}{
+		{
+			name: "success",
+			setup: func() {
+
+			},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			s.ctrl = gomock.NewController(t)
+			s.setupTest()
+			scenario.setup()
+
+			response := s.random.GenerateRandomString()
+			assert.Equal(t, len(response), shortURLLength)
+
+		})
+	}
+
+}
