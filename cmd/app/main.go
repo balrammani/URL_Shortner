225d9dfbda7bc165d45ae8097a10f4f61f731221
Balaraman Mani <bmani1@ext.uber.com>
2022-01-30 18:32:07 +0530

    testing random generated string

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,13 @@
-package app
+package main
+
+import (
+	"fmt"
+	randomgenerator "github.com/balram/rest-url-shortner/utils/random_generator"
+	"log"
+)
+
+func main(){
+	log.Println("Starting rest service - url shortner")
+	random,_:=randomgenerator.New()
+	fmt.Println(random.GenerateRandomString())
+}
